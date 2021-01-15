7f7a617a8ebb91e218f1062df5dd56e30053fdd7
khingz <kingsley995mr@gmail.com>
2021-01-15 14:04:41 -0400

    rough finish

--- index.js
+++ index.js
@@ -1,20 +1,58 @@
 const express = require('express');
 const fetch = require('node-fetch');
 
-
+//initialize express app
 const app = express();
 
+//store fetch request parameters in variables
 let url = 'https://api.exchangeratesapi.io/latest';
 let settings = { method: 'Get' };
 
 
 
-app.get('/api/rates?base', (req, res) => {
+app.get('/api/rates', (req, res) => {
+    //fetch json
     fetch(url, settings)
     .then(res => res.json())
     .then( data => {
-        res.json(data)
-        console.log(data);
+        //get query parameters
+        let queryParams = req.query;
+        let base = queryParams.base;
+        let currency = queryParams.currency;
+
+        let rateRes = data.rates;
+
+    
+        //output
+        let output = {
+            results: {
+                base: base,
+                date: new Date().toLocaleDateString(),
+                currency: {}
+            }
+        }
+
+        //split currency query
+        let currencyList = currency.split(',');
+        
+        //loop through query and create currency object
+        currencyList.forEach(cur => {
+            output.results.currency[`${cur}`] = data.rates[cur]
+        })  
+
+        if((base === undefined) || (base === '') || (currency === undefined) || (currency === '')) {
+            res.status('400')
+            res.json({message: 'not found'})
+        } else {
+            res.json(output)
+        }
+        // if(rateRes[`${base}`]) {
+        //     res.json(output)
+        // } else {
+        //     res.status('400')
+        //     res.json({message: 'not found'})
+        // }
+        // res.json(output)
     })  
 })
 
